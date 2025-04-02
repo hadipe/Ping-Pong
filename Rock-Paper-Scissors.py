071242fd50a78053fcc65391a2bdd8cd1d13ee59
Technology Rooms <technologyrooms@colsanjose.edu.co>
2025-04-02 09:10:57 -0500

    asdfd

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,18 @@
+import random
+
+options = ("rock", "paper", "scissors")
+running = True
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print
