a96d6edb1060fd7e0c57dd0b42c99278a4a41d79
Technology Rooms <technologyrooms@colsanjose.edu.co>
2025-04-25 10:08:01 -0500

    haffid

--- pong.py
+++ pong.py
@@ -2,30 +2,45 @@ import pygame
 import sys
 import random
 
-# Inicializar Pygame y el mezclador de audio
+# Inicializar Pygame y audio
 pygame.init()
 pygame.mixer.init()
 
-# Música de fondo
-pygame.mixer.music.load("musica_fondo.mp3")
-pygame.mixer.music.set_volume(0.5)
-pygame.mixer.music.play(-1)
+# Lista de canciones (playlist)
+playlist = [
+    "musica_fondo1.mp3",
+    "musica_fondo2.mp3",
+    "musica_fondo3.mp3"
+]
+current_song = 0
+
+# Evento para detectar fin de canción
+pygame.mixer.music.set_endevent(pygame.USEREVENT + 1)
+
+def play_next_song():
+    global current_song
+    pygame.mixer.music.load(playlist[current_song])
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play()
+    current_song = (current_song + 1) % len(playlist)
+
+play_next_song()  # Reproducir la primera canción
 
 # Cargar sonidos
 hit_sound = pygame.mixer.Sound("hit.wav.mp3")
 score_sound = pygame.mixer.Sound("score.wav.mp3")
-power_sound = pygame.mixer.Sound("power.wav.mp3")  # Sonido genérico para cualquier poder
+power_sound = pygame.mixer.Sound("power.wav.mp3")
 
 # Configurar pantalla
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pong con Poderes")
 
-# Cargar imagen de fondo
+# Imagen de fondo
 background_image = pygame.image.load("fondo_pong.jpg").convert()
 background_image = pygame.transform.scale(background_image, (width, height))
 
-# Cargar imágenes de poderes
+# Imágenes de poderes
 power_images = {
     "speed": pygame.image.load("power_speed.png"),
     "big_paddle": pygame.image.load("power_big_paddle.png"),
@@ -41,16 +56,14 @@ for key in power_images:
 # Colores
 white = (255, 255, 255)
 
-# Paletas
+# Paletas y pelota
 paddle_width, paddle_height = 10, 100
 paddle_speed = 10
 
-# Pelota
 ball_size = 20
 ball_speed_x = 5 * random.choice((1, -1))
 ball_speed_y = 5 * random.choice((1, -1))
 
-# Objetos
 paddle_a = pygame.Rect(50, height//2 - paddle_height//2, paddle_width, paddle_height)
 paddle_b = pygame.Rect(width - 50 - paddle_width, height//2 - paddle_height//2, paddle_width, paddle_height)
 ball = pygame.Rect(width//2 - ball_size//2, height//2 - ball_size//2, ball_size, ball_size)
@@ -78,6 +91,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.USEREVENT + 1:
+            play_next_song()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+            play_next_song()  # Cambio manual de canción
 
     # Movimiento paletas
     keys = pygame.key.get_pressed()
@@ -91,7 +108,7 @@ while True:
     if keys[pygame.K_DOWN] and paddle_b.bottom < height:
         paddle_b.y += paddle_speed
 
-    # Mover pelota(s)
+    # Movimiento pelota(s)
     ball.x += int(ball_speed_x)
     ball.y += int(ball_speed_y)
     if second_ball:
@@ -132,7 +149,6 @@ while True:
 
     if power_active and ball.colliderect(power):
         power_sound.play()
-
         if power_type == "speed":
             ball_speed_x *= 1.5
             ball_speed_y *= 1.5
@@ -150,17 +166,15 @@ while True:
         elif power_type == "invisible":
             invisible = True
             invisible_timer = pygame.time.get_ticks()
-
         power_active = False
         power_timer = pygame.time.get_ticks()
 
-    # Restaurar efectos después de 5 segundos
+    # Restaurar efectos
     if pygame.time.get_ticks() - power_timer > 5000:
         paddle_a.height = 100
         ball.width = ball.height = 20
         second_ball = None
         invisible = False
-
     if frozen and pygame.time.get_ticks() - frozen_timer > 3000:
         frozen = False
 
@@ -168,17 +182,13 @@ while True:
     screen.blit(background_image, (0, 0))
     pygame.draw.rect(screen, white, paddle_a)
     pygame.draw.rect(screen, white, paddle_b)
-
     if not invisible:
         pygame.draw.ellipse(screen, white, ball)
     if second_ball:
         pygame.draw.ellipse(screen, white, second_ball)
-
     if power_active:
         screen.blit(power_images[power_type], (power.x, power.y))
-
     score_display = font.render(f"Jugador A: {score_a}  Jugador B: {score_b}", True, white)
     screen.blit(score_display, (width // 2 - score_display.get_width() // 2, 20))
-
     pygame.display.flip()
     clock.tick(60)
